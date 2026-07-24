ba3f4834506acaae8023fe7d3ccfca8aa86a1f7a
Matheus Ramos <132701491+mattheusramos@users.noreply.github.com>
2026-07-24 18:17:59 -0300

    feito Screen e ViewModel da Home

--- app/src/main/java/com/matheus/pokeapp/home/HomeScreen.kt
+++ app/src/main/java/com/matheus/pokeapp/home/HomeScreen.kt
@@ -2,62 +2,115 @@ package com.matheus.pokeapp.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.matheus.pokeapp.R
-import com.matheus.pokeapp.login.LoginScreen
 import com.matheus.pokeapp.utils.*
+import com.matheus.pokeapp.ui.components.PokemonCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
-    onPokemonClick: () -> Unit,
+    viewModel: HomeViewModel = viewModel(),
+    onPokemonClick: (Int) -> Unit,
     onSettingsClick: () -> Unit,
 ){
+
+    val pokemons by viewModel.pokemons.collectAsState()
+    val loading by viewModel.isLoading.collectAsState()
+    val error by viewModel.error.collectAsState()
 
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
-                    Image(
-                        painter = painterResource(id = R.drawable.pokeball_icon),
-                        contentDescription = "Pokeball Icon",
-                        modifier = Modifier
-                            .size(24.dp)
-                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.pokeball_icon),
+                            contentDescription = null,
+                            modifier = Modifier.size(28.dp)
+                        )
 
-                    Text(
-                        text = "Pokedex",
-                        fontWeight = FontWeight.Bold,
-                        color = VermelhoPrincipal,
-                        modifier = Modifier
-                            .padding(horizontal = 32.dp)
-                    )
+                        Spacer(modifier = Modifier.width(8.dp))
 
+                        Text(
+                            text = "Pokedex",
+                            color = VermelhoPrincipal,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 }
             )
         }
         // bottombar = {}
     ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .padding(paddingValues)
-        ) {
-            // Content for the Pokemon list will go here
+
+        when {
+            loading -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            error != null -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(error ?: "")
+                }
+            }
+
+            else -> {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+
+                    items(pokemons) { pokemon ->
+                        PokemonCard(
+                            pokemon = pokemon,
+                            onClick = {
+                                //onPokemonClick(Int)
+                            }
+                        )
+
+                    }
+
+                }
+            }
+
         }
     }
+
 }
 
-@Preview
-@Composable
-fun HomeScreenPreview(){
-    HomeScreen(
-        onPokemonClick = {  },
-        onSettingsClick = { }
-    )
-}
+//@Preview
+//@Composable
+//fun HomeScreenPreview(){
+//    HomeScreen(
+//        onPokemonClick = {  },
+//        onSettingsClick = { }
+//    )
+//}
--- app/src/main/java/com/matheus/pokeapp/login/LoginScreen.kt
+++ app/src/main/java/com/matheus/pokeapp/login/LoginScreen.kt
@@ -3,23 +3,10 @@ package com.matheus.pokeapp.login
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,8 +14,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
 import com.matheus.pokeapp.R
 import com.matheus.pokeapp.utils.*
 
@@ -77,7 +63,11 @@ fun LoginScreen(
         )
 
         Button(
-            onClick = { onLoginClick(email, password) },
+            onClick = {
+                if (email == "teste" && password == "123456") {
+                    onLoginClick(email, password)
+                }
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp, vertical = 36.dp)
@@ -88,7 +78,7 @@ fun LoginScreen(
             Text(
                 text = "Login",
                 fontWeight = FontWeight.Bold,
-                fontSize = 16.sp
+                fontSize = 13.sp
             )
         }
 
